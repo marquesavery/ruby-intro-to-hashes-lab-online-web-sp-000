cb897fcffc1680ff98df46cce100a8e99134c94c
Marques Avery <averymarques4@gmail.com>
2020-08-11 17:52:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,7 @@ def monopoly_with_second_tier
 		{
 			pieces: 4,
 			names: {},
-			rent_in_dollars: {
-				one_piece_owned: 25,
-				two_pieces_owned: 50
-			}
+			rent_in_dollars: {}
 			}
 		}
 
