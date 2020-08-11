c3f4ae4d88fc6a0275e4b596291b089e3cc30cfd
Marques Avery <averymarques4@gmail.com>
2020-08-11 17:57:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,9 +40,10 @@ def monopoly_with_third_tier
 				four_pieces_owned: 200
 				}
 		}
+	}
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
+
 end
